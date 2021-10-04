0a20ab438b42cabb9ac323ef78add97f9c060c76
rodrigosuelli <rodrigosuellli@gmail.com>
2021-10-04 17:20:45 -0300

    Add ex08

---
+++ ex08.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include "get_input.h"
+
+int main()
+{
+  // Cria matriz
+  int matriz[7][7] = {0};
+
+  // Gera matriz
+  for (int linha = 0; linha < 7; linha++)
+  {
+    for (int coluna = 0; coluna < 7; coluna++)
+    {
+      if (coluna == linha)
+      {
+        matriz[linha][coluna] = 1;
+      }
+    }
+  }
+
+  // Printa matriz
+  puts("Matrix Identidade Gerada:");
+
+  for (int linha = 0; linha < 7; linha++)
+  {
+    printf("|");
+    for (int coluna = 0; coluna < 7; coluna++)
+    {
+      printf("%d", matriz[linha][coluna]);
+
+      if (coluna != 6)
+      {
+        printf(", ");
+      }
+    }
+    printf("|\n");
+  }
+
+  return 0;
+}
